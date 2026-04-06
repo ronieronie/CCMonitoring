c4e21c1f377e43922d33393c8a7b136ce0dffc75
ronieronie <150136563+ronieronie@users.noreply.github.com>
2026-04-06 14:02:09 +0800

    Replace local Bootstrap CSS with CDN version

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,13 +21,14 @@
     <script src="{{asset('js/authentication-main.js')}}"></script>
 
     <!-- Bootstrap Css -->
-    <link id="style" href="{{ asset('libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
 
     <!-- Style Css -->
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
 
     <!-- Icons Css -->
-    <link href="{{ asset('css/icons.css') }}" rel="stylesheet" />
     <style>
         .custom-page .header-logo img {
             height: 5rem;
