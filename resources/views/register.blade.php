1468b05e0bc27f953e8268e49cf3097b74b7f6d8
ronieronie <150136563+ronieronie@users.noreply.github.com>
2026-06-09 17:51:54 +0800

    asdasd

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -526,11 +526,11 @@
                         <div class="row">
                             <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
                                 <div class="card-sigin">
-                                    <!-- <div class="mb-5 d-flex">
+                                    <div class="mb-5 d-flex">
                                         <a href="index.html" class="header-logo"><img src="bms_logo.png"
                                                 class="desktop-logo ht-40" alt="logo" />                                          
                                         </a>
-                                    </div> -->
+                                    </div>
                                     <div class="card-sigin">
 
                                         <div class="main-signup-header">
